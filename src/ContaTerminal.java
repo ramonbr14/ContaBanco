e0f421930e9c4fab5ab578b9c15bb72adfa0be09
ramonbr14 <ramonbr14@gmail.com>
2023-09-21 23:29:53 -0400

    Iniciando o Desafio Java-Basico!

---
+++ src/ContaTerminal.java
@@ -0,0 +1,8 @@
+public class ContaTerminal{
+    public static void main(String[] args) throws Exception {
+            int numero;
+            String agencia;
+            String nomeCliente;
+            double saldo;
+    }
+}
